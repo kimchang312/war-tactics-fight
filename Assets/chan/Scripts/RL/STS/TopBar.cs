0ce6aa26214107c5ff5834e038d6b163e4de8e44
ElixSaes <agechan@naver.com>
2025-05-20 23:31:54 +0900

    chan
    설정창 완료(저장 메서드 제외)

--- Assets/chan/Scripts/RL/STS/TopBar.cs
+++ Assets/chan/Scripts/RL/STS/TopBar.cs
@@ -17,12 +17,18 @@ public class TopBar : MonoBehaviour
     [SerializeField] private Button upgradeStatusButton;
     [SerializeField] private Button closeupgradeStatusButton;
 
-
     [Header("Academy Panel Toggle")]
     [SerializeField] private GameObject academyPanel;
     [SerializeField] private Button academyToggleButton;
+
+    [Header("Option Panel Toggle")]
+    [SerializeField] private GameObject optionPanel;
+    [SerializeField] private Button optionButton;
+    [SerializeField] private Button continueButton;
+    [SerializeField] private Button saveAndGoTitleButton;
     private void Awake()
     {
+        optionPanel?.SetActive(false);
         // 초기에는 다 꺼두고
         ownedRelicsPanel?.SetActive(false);
         upgradePanel?.SetActive(false);
@@ -36,6 +42,11 @@ public class TopBar : MonoBehaviour
         // 닫기 버튼에도 같은 토글 메서드 연결
         closeRelicsButton.onClick.AddListener(() => ToggleOnly(ownedRelicsPanel));
         closeupgradeStatusButton.onClick.AddListener(() => ToggleOnly(upgradeStatusPanel));
+
+        // 옵션 관련 버튼 연결
+        optionButton?.onClick.AddListener(() => ToggleOptionPanel(true));
+        continueButton?.onClick.AddListener(() => ToggleOptionPanel(false));
+        saveAndGoTitleButton?.onClick.AddListener(SaveAndGoTitle);
     }
     
     private void ToggleOnly(GameObject panel)
@@ -51,8 +62,17 @@ public class TopBar : MonoBehaviour
         // 클릭 직전 꺼져 있었다면 켜고, 켜져 있었다면 그대로 꺼두기
         panel.SetActive(!wasActive);
     }
-    private void closePanel()
+    private void ToggleOptionPanel(bool show)
     {
-        
+        optionPanel?.SetActive(show);
+    }
+
+    private void SaveAndGoTitle()
+    {
+        Debug.Log(" 게임 저장 중");
+        //저장하는 함수
+
+        Debug.Log("타이틀 씬으로 이동 중");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Title"); // 씬 이름이 정확해야 함
     }
 }
