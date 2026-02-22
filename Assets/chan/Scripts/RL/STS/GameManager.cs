39147ca38b73d0fc99c21f69881821a51e1dde10
ElixSaes <agechan@naver.com>
2026-02-22 20:11:07 +0900

    chan

--- Assets/chan/Scripts/RL/STS/GameManager.cs
+++ Assets/chan/Scripts/RL/STS/GameManager.cs
@@ -347,6 +347,11 @@ private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
                 
                 // 전장 효과를 fieldId로 설정 (AbilityManager에서 사용)
                 int fieldId = MapGenerator.GetFieldIdFromBattlefieldEffect(newStage.battlefieldEffect);
+                int nowField = RogueLikeData.Instance.GetFieldId();
+                if(nowField != 0)
+                {
+                    fieldId = nowField;
+                }
                 RogueLikeData.Instance.SetFieldId(fieldId);
             }
             
